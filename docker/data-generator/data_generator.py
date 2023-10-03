f67bc9540e459d16b77f1ec566c200ec45315ff8
Haeun-Oh <haeunoh.tech@gmail.com>
2023-10-03 22:44:13 +0900

    fix: format

--- docker/data-generator/data_generator.py
+++ docker/data-generator/data_generator.py
@@ -19,28 +19,35 @@ BOTO3_CLIENT = boto3.client(
     aws_secret_access_key=MINIO_SECRET_ACCESS_KEY,
 )
 
+
 @ray.remote(max_calls=1)
 def download_upload_data(file_name):
     root_url = "https://data.rees46.com/datasets/marketplace"
     url = os.path.join(root_url, file_name)
     subprocess.run(["axel", url])
-    
-    with gzip.open(file_name, 'rb') as gzipped_file:
-       df = pd.read_csv(gzipped_file)
+
+    with gzip.open(file_name, "rb") as gzipped_file:
+        df = pd.read_csv(gzipped_file)
     os.remove(file_name)
     return df
-    
+
+
 if __name__ == "__main__":
     file_name_list = [
-                        "2019-Oct.csv.gz", "2019-Nov.csv.gz", "2019-Dec.csv.gz",
-                        "2020-Jan.csv.gz", "2020-Feb.csv.gz", "2020-Mar.csv.gz", "2020-Apr.csv.gz"
-                      ]
-    
+        "2019-Oct.csv.gz",
+        "2019-Nov.csv.gz",
+        "2019-Dec.csv.gz",
+        "2020-Jan.csv.gz",
+        "2020-Feb.csv.gz",
+        "2020-Mar.csv.gz",
+        "2020-Apr.csv.gz",
+    ]
+
     ray.init(log_to_driver=False)
     obj_id = [download_upload_data.remote(file_name) for file_name in file_name_list]
     raw_df_list = ray.get(obj_id)
     total_raw_df = pd.concat(raw_df_list, ignore_index=True)
-    
+
     spark = configure_spark_with_delta_pip(SparkSession.builder).getOrCreate()
     spark_df = spark.createDataFrame(total_raw_df)
     (
@@ -51,4 +58,3 @@ if __name__ == "__main__":
         .partitionBy("user_id")
         .saveAsTable("ecommerce_table")
     )
-    
