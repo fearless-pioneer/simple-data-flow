9374f5c0e6b113431765494a43d49a3387828447
Haeun-Oh <haeunoh.tech@gmail.com>
2023-10-03 16:16:54 +0900

    add: data download

---
+++ docker/data-generator/data_generator.py
@@ -0,0 +1,38 @@
+import boto3
+import os
+import wget
+import gzip
+
+MINIO_BUCKET = "data"
+MINIO_ENDPOINT = "http://minio:9000"
+MINIO_ACCESS_KEY_ID = "minio"
+MINIO_SECRET_ACCESS_KEY = "minio123"
+
+if __name__ == "__main__":
+    BOTO3_CLIENT = boto3.client(
+        "s3",
+        endpoint_url=MINIO_ENDPOINT,
+        aws_access_key_id=MINIO_ACCESS_KEY_ID,
+        aws_secret_access_key=MINIO_SECRET_ACCESS_KEY,
+    )
+
+    root_url = "https://data.rees46.com/datasets/marketplace"
+    file_name_list = [
+                        "2019-Oct.csv.gz", "2019-Nov.csv.gz", "2019-Dec.csv.gz",
+                        "2020-Jan.csv.gz", "2020-Feb.csv.gz", "2020-Mar.csv.gz", "2020-Apr.csv.gz"
+                      ]
+    for file_name in file_name_list:
+        url = os.path.join(root_url, file_name)
+        wget.download(url)
+        
+        with gzip.open(file_name, 'rb') as f:
+            csv_file_data = f.read().decode('utf-8')
+        
+        csv_file_name = file_name[:-3]    
+        with open(csv_file_name, 'w', encoding='utf-8') as csv_file:
+            csv_file.write(csv_file_data)
+            
+        BOTO3_CLIENT.upload_file(csv_file_name, MINIO_BUCKET, csv_file_name)
+        
+        os.remove(file_name)
+        os.remove(csv_file_name)
