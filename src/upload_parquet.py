ee963835c0fa4da1962f4a647b0e146993f99d44
Haeun-Oh <haeunoh.tech@gmail.com>
2023-10-06 11:56:39 +0900

    fix: format

--- src/upload_parquet.py
+++ src/upload_parquet.py
@@ -2,11 +2,6 @@
 df = spark.read.format("csv").load("s3a://data/2019-Oct.csv", header=True)
 
 # %%
-(
-    df
-    .write.mode("overwrite")
-    .option("compression", "snappy")
-    .parquet("s3a://data/data.parquet")
-)
+(df.write.mode("overwrite").option("compression", "snappy").parquet("s3a://data/data.parquet"))
 
 # %%
