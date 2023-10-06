4cda61888e092e21f6f5a39877aedebc4e84b52a
Haeun-Oh <haeunoh.tech@gmail.com>
2023-10-06 11:52:10 +0900

    add: upload spark parquet

---
+++ src/upload_parquet.py
@@ -0,0 +1,12 @@
+# %%
+df = spark.read.format("csv").load("s3a://data/2019-Oct.csv", header=True)
+
+# %%
+(
+    df
+    .write.mode("overwrite")
+    .option("compression", "snappy")
+    .parquet("s3a://data/data.parquet")
+)
+
+# %%
