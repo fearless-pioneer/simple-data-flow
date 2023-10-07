aa51d5b675b151cdcb1bff75813b5974e6bdd7cb
Haeun-Oh <haeunoh.tech@gmail.com>
2023-10-07 09:06:47 +0900

    add: raw

--- src/upload_parquet.py
+++ src/upload_parquet.py
@@ -1,4 +1,4 @@
 # %%
-df = spark.read.format("csv").load("s3a://data/", header=True)  # noqa: F821
+df = spark.read.format("csv").load("s3a://data/raw", header=True)  # noqa: F821
 
 df.write.mode("overwrite").option("compression", "snappy").parquet("s3a://data/lake/spark_df.parquet")
