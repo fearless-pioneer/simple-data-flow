7c629ce77e913f9af5d84c755bab8e8ee84388f7
Haeun Oh <haeunoh.tech@gmail.com>
2023-10-08 21:03:25 +0900

    Feature/data generate (#12)
    * add: data download
    
    * add: data generate
    
    * add: end of file
    
    * add: axel & ray
    
    * fix: format
    
    * add: packages to be installed
    
    * add: upload csv file to minio
    
    * add: upload spark parquet
    
    * fix: format
    
    * delete: bracket
    
    Co-authored-by: Dongmin Lee <kid33629@gmail.com>
    
    * delete: useless line
    
    * fix: pip install
    
    * add: noqa
    
    * apply: format
    
    * add: data
    
    * delete: white space
    
    * fix: format
    
    * add: minio folder
    
    * delete: useless line
    
    * update: data generator
    
    * add: raw
    
    * add: white space
    
    * update: README.md
    
    ---------
    
    Co-authored-by: Dongmin Lee <kid33629@gmail.com>

---
+++ docker/data-generator/data_generator.py
@@ -0,0 +1,32 @@
+import boto3
+import os
+
+MINIO_BUCKET = "data"
+MINIO_ENDPOINT = "http://minio:9000"
+MINIO_ACCESS_KEY_ID = "minio"
+MINIO_SECRET_ACCESS_KEY = "minio123"
+
+BOTO3_CLIENT = boto3.client(
+    "s3",
+    endpoint_url=MINIO_ENDPOINT,
+    aws_access_key_id=MINIO_ACCESS_KEY_ID,
+    aws_secret_access_key=MINIO_SECRET_ACCESS_KEY,
+)
+
+
+def main():
+    root_file_path = "./data"
+    file_name_list = ["2019-Oct.csv", "2019-Dec.csv"]
+
+    for file_name in file_name_list:
+        local_csv_file_path = os.path.join(root_file_path, file_name)
+        minio_file_path = f"raw/{file_name}"
+        BOTO3_CLIENT.upload_file(
+            local_csv_file_path,
+            MINIO_BUCKET,
+            minio_file_path,
+        )
+
+
+if __name__ == "__main__":
+    main()
