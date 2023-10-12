d994df688973254366bcfebb79bc327bfa84776e
dongminlee94 <kid33629@gmail.com>
2023-10-13 00:33:03 +0900

    feat: add base code

--- src/upload_parquet.py
+++ src/upload_parquet.py
@@ -1,4 +1,12 @@
 # %%
-df = spark.read.format("csv").load("s3a://data/raw", header=True)  # noqa: F821
+from delta import configure_spark_with_delta_pip
+from pyspark.sql import SparkSession
 
-df.write.mode("overwrite").option("compression", "snappy").parquet("s3a://data/lake/spark_df.parquet")
+# %%
+spark = configure_spark_with_delta_pip(SparkSession.builder).getOrCreate()
+
+# %%
+df = spark.read.format("csv").load("s3a://data/raw/", header=True)
+
+# %%
+df.write.mode("overwrite").option("compression", "snappy").parquet("s3a://data/lake", compression="snappy")
