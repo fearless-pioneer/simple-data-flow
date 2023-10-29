4876907e46793414b44850c3a4eb156748687cd5
Haeun-Oh <haeunoh.tech@gmail.com>
2023-10-29 19:19:38 +0900

    add: format

--- docker/data-uploader/data_uploader.py
+++ docker/data-uploader/data_uploader.py
@@ -1,7 +1,9 @@
 from delta import configure_spark_with_delta_pip
 from pyspark.sql import SparkSession
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     spark = configure_spark_with_delta_pip(SparkSession.builder).getOrCreate()
     df = spark.read.format("csv").load("s3a://data/raw/", header=True)
-    df.write.mode("overwrite").option("compression", "snappy").parquet("s3a://data/lake", compression="snappy")
+    df.write.mode("overwrite").option("compression", "snappy").parquet(
+        "s3a://data/lake", compression="snappy"
+    )
