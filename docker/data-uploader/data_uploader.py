6ff638b2c30b636b2910efaf18aa22d140dcef45
Haeun-Oh <haeunoh.tech@gmail.com>
2023-10-29 21:24:10 +0900

    add: lint

--- docker/data-uploader/data_uploader.py
+++ docker/data-uploader/data_uploader.py
@@ -5,5 +5,6 @@ if __name__ == "__main__":
     spark = configure_spark_with_delta_pip(SparkSession.builder).getOrCreate()
     df = spark.read.format("csv").load("s3a://data/raw/", header=True)
     df.write.mode("overwrite").option("compression", "snappy").parquet(
-        "s3a://data/lake", compression="snappy"
+        "s3a://data/lake",
+        compression="snappy",
     )
